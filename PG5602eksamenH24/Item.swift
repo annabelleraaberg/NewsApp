57f6cca89e236e39048e6c49ca2e1de5a0726de4
annabelleraaberg <129503939+annabelleraaberg@users.noreply.github.com>
2024-11-21 18:24:08 +0100

    Initial Commit

---
+++ PG5602eksamenH24/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PG5602eksamenH24
+//
+//  Created by Annabelle Deichmann Raaberg on 21/11/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
